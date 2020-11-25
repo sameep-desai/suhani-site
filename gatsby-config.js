384253fe0f50d355ffb4fd224b26a838b89f1b16
Ashique-Desai <desai.ashique.com>
2020-11-25 11:42:57 +0530

    Heading and title changed - gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
+    title: `Suhani constructions`,
     name: `Narative`,
     siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `We build to last for a lifetime. Welcome to Suhani constructions.`,
       maxWidth: 652,
     },
     social: [
